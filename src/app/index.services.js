6ccb8f7cb4c03dfa6aa27a69fecf157fb1193842
carlo.bonamico@gmail.com <carlo.bonamico@gmail.com>
2015-07-01 17:11:53 +0200

    service

--- src/app/index.services.js
+++ src/app/index.services.js
@@ -12,16 +12,19 @@
     
     var expenses = [
       {
+        employee: "carlo",
         date: "10/1/2015", 
         type: "train", 
         amount : "10"
       },
       {
+        employee: "carlo",
         date: "10/1/2015", 
         type: "hotel", 
         amount : "100"
       },
       {
+        employee: "marco",
         date: "10/1/2015", 
         type: "taxi", 
         amount : "19"
@@ -32,6 +35,9 @@
       return expenses; 
     };
     
+    this.getExpensesForEmployee = function (employee){
+      return expenses; 
+    };
   }
 
 })();
